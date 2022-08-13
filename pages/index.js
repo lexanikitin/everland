63a1148c7fdad8d22a8d62e9e3ba3800e03b93e1
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-08-13 12:00:43 +0300

    feat: включение и выключение меню второго уровня

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-//Появление
+//Объявляем переменные и константы
+const content = document.querySelector('.page');
+const header = content.querySelector('.header')
+const menuButton = content.querySelector('.header__menu-button');
+const menuItems = content.querySelectorAll('.header__checkbox-button-big-menu');
+const iconBurgerMenu = content.querySelector('.header__menu-button_type_burger');
+const iconCloseMenu = content.querySelector('.header__menu-button_type_close');
+const bigMenu = content.querySelector('.header__big-menu');
+
+//Обработка события click при нажитии на кпонку бургер меню
+function toggleBigMenu() {
+  bigMenu.classList.toggle('header__big-menu_opened');
+  iconBurgerMenu.classList.toggle('header__menu-button_opened');
+  iconCloseMenu.classList.toggle('header__menu-button_opened');
+  header.classList.toggle('header_shadow');
+}
+// Обработка включения и выключения большого меню
+menuButton.addEventListener('click', toggleBigMenu);
