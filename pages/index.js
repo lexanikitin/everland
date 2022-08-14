04b42cabbadd779f12cc0aa2856bdb0d5dd8e9b1
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2022-08-14 15:08:19 +0300

    fix: поправил баг с кнопкой раскрытия в подменю меню второго уровня

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-//Объявляем переменные и константы
+//Объявляем константы
 const content = document.querySelector('.page');
 const header = content.querySelector('.header')
 const menuButton = header.querySelector('.header__menu-button');
@@ -6,6 +6,7 @@ const iconBurgerMenu = header.querySelector('.header__menu-button_type_burger');
 const iconCloseMenu = header.querySelector('.header__menu-button_type_close');
 const bigMenu = header.querySelector('.header__big-menu');
 const menuItems = bigMenu.querySelectorAll('.header__checkbox-button-big-menu');
+const shevronIcons = bigMenu.querySelectorAll('.header__icon-shevron');
 const menuLists = bigMenu.querySelectorAll('.header__big-menu-list');
 
 // фукнция обработки нажития на кпонку бургер меню
@@ -16,20 +17,30 @@ function toggleBigMenu() {
   header.classList.toggle('header_shadow');
 }
 
-// фукнция обработки нажития на подпункт меню второго уровня
+// фукнция обработки нажатия на подпункт меню второго уровня
 function toggleMenuItems() {
   const menuItem = event.target.closest('.header__big-menu-lists');
   const menuList = menuItem.querySelector('.header__big-menu-list');
+  const iconShevronActive = menuItem.querySelector('.header__icon-shevron');
+  shevronIcons.forEach((icon) => {
+    if (icon === iconShevronActive) {
+      icon.classList.toggle('header__icon-shevron_rotate');
+    } else {
+      icon.classList.remove('header__icon-shevron_rotate');
+    }
+  });
+
   menuList.classList.toggle('header__big-menu-list_opened');
-  menuLists.forEach((list) =>{
-    if(list !== menuList) {
+  menuLists.forEach((list) => {
+    if (list !== menuList) {
       list.classList.remove('header__big-menu-list_opened');
     }
   });
 }
+
 // обработка события click при нажитии на кпонку бургер меню
 menuButton.addEventListener('click', toggleBigMenu);
 
 // обработка события click при нажитии на раздел меню второго уровня
-menuItems.forEach((menuItem)  =>
-  menuItem.addEventListener('click', () => toggleMenuItems()));
+menuItems.forEach((menuItem) =>
+  menuItem.addEventListener('click', toggleMenuItems));
