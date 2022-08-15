4cca0ea631ff97f1b8143a267ffe63cb74f92eef
Aleksandr Stepanenko <alalstepan@gmail.com>
2022-08-15 22:05:54 +1200

    feat: написан скрипт раскрытия аккордеонов

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,50 @@
-const openButton = document.querySelector('.button-accordion');
-const text = document.querySelector('.accordion__text');
+const advantagesSectionNode = document.querySelector(
+  "#advantages .accordion__column"
+);
+let currentTextNode = null;
+advantagesSectionNode
+  .querySelectorAll(".button-accordion")
+  .forEach((currentButtonNode) => {
+    currentButtonNode.addEventListener("click", (event) => {
+      const buttonNode = event.target.closest(".button-accordion");
+      if (
+        !buttonNode.classList.contains("button-accordion_active") &&
+        currentTextNode !== null
+      ) {
+        currentTextNode
+          .closest(".accordion")
+          .querySelector(".button-accordion")
+          .classList.remove("button-accordion_active");
+        currentTextNode.classList.add("accordion__text_hidden");
+        currentTextNode.style.maxHeight = 0;
+      }
+      buttonNode.classList.toggle("button-accordion_active");
+      currentTextNode = buttonNode
+        .closest(".accordion")
+        .querySelector(".accordion__text");
+      currentTextNode.classList.toggle("accordion__text_hidden");
+      if (currentTextNode.classList.contains("accordion__text_hidden")) {
+        currentTextNode.style.maxHeight = 0;
+      } else
+        currentTextNode.style.maxHeight =
+          currentTextNode.scrollHeight + 20 + "px"; //20px - верхний паддинг
+    });
+  });
 
-openButton.addEventListener("click", ()=>{
-  text.classList.toggle('accordion__text_hidden');
-  openButton.classList.toggle('button-accordion_active');
-  if(text.classList.contains('accordion__text_hidden')){
-    text.style.maxHeight = 0;
-  } else text.style.maxHeight = text.scrollHeight + 20 +'px';
-  console.log(text.scrollHeight);
-});
-
-window.addEventListener('resize', function(event) {
-  if(text.classList.contains('accordion__text_hidden')){
-    text.style.maxHeight = 0;
-  } else text.style.maxHeight = text.scrollHeight + 20 +'px';
-  console.log(text.scrollHeight);
-}, true);
+//Из-за анимации не используется свойство fit-content
+//Событие resize необходимо, чтобы пересчитывать высоту текстовых блоков
+//например при переходе на смартфоне из вертикального в
+//горизонтальное ориентирование экрана
+window.addEventListener(
+  "resize",
+  function () {
+    advantagesSectionNode
+      .querySelectorAll(".accordion__text")
+      .forEach((textNode) => {
+        if (textNode.classList.contains("accordion__text_hidden")) {
+          textNode.style.maxHeight = 0;
+        } else textNode.style.maxHeight = textNode.scrollHeight + 20 + "px";
+      });
+  },
+  true
+);
