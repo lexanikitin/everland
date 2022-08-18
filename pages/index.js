6fa293ddd8c8ad73f5a250375db7b2187e250290
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2022-08-18 21:20:13 +0300

    added lost styles form numbers section

--- pages/index.js
+++ pages/index.js
@@ -236,3 +236,21 @@ function slider(sliderBlockClass, sliderCaseClass, sliderItemClass, sliderItemAc
     currentPage.textContent = index + 1;
   };
 };
+
+// const paymentButtons = document.querySelectorAll('.supp')
+// const sliderWrapper = document.querySelector('.support-form__button-wrapper_type_payment')
+// let count = 0;
+// let width;
+
+// function slider() {
+//   console.log('resize');
+//   width = sliderWrapper.offsetWidth;
+//   sliderWrapper.style.width = width.paymentButtons.length + 'px';
+//   paymentButtons.forEach((item) => {
+//     item.style.width = width + 'px';
+//     item.style.height = 'auto';
+//   })
+//   console.log(width);
+// }
+// window.addEventListener('resize', slider)
+// slider();
